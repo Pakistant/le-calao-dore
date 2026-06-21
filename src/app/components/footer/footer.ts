4f17b5b1eb1c18477f66d66ae1ac547e3bf9fe84
steve pakistant <stdouanla@gmail.com>
2026-06-21 16:48:43 +0100

    feat(footer): footer responsive 3 colonnes

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer {
+  annee = new Date().getFullYear();
+
+  liens = ['Menu', 'Horaires', 'Contact'];
+
+  reseaux = [
+    { nom: 'Facebook',  icone: '📘', url: '#' },
+    { nom: 'Instagram', icone: '📸', url: '#' },
+    { nom: 'WhatsApp',  icone: '💬', url: '#' },
+  ];
+}
